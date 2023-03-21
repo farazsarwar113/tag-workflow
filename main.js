274b916dec1486334bca8c11a43936fb5298f9bd
Faraz Sarwar <faraz@sudofy.com>
2023-03-21 11:06:53 +0500

    Workflow added

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
 console.log('Hello World!');
 console.log('This is feature 1');
+
+// # gh api \ -H "Accept: application/vnd.github+json" \ -H "X-GitHub-Api-Version: 2022-11-28" \ /repos/{{GITHUB_REPOSITORY}}/git/matching-refs/tags
